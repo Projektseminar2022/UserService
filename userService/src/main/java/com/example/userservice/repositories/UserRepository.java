02a4547d507883cad4d3e5b24374268cace2f263
Saad Belkhou <saad.belkhou@gmail.com>
2022-11-24 17:13:44 +0100

    changed JpaRepository to ReactiveCrudRepository

--- userService/src/main/java/com/example/userservice/repositories/UserRepository.java
+++ userService/src/main/java/com/example/userservice/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.example.userservice.repositories;
 
 import com.example.userservice.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+
+public interface UserRepository extends ReactiveCrudRepository<User,Integer> {
 
 }
