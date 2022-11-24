f2310225e0d1f15526422c0fba81fc3abc8a9628
Karmen Voncina <karmen.voncina@smail.inf.h-brs.de>
2022-11-24 22:47:23 +0100

    Datenbankkonfiguration

--- userService/src/main/java/services/UserService.java
+++ userService/src/main/java/services/UserService.java
@@ -2,13 +2,14 @@ package services;
 
 
 import entities.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.UserRepository;
 
 import java.util.List;
 
-
+@Slf4j
 @Service
 public class UserService {
 
