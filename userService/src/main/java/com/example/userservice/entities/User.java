5cdbcbf2673c584788b47ad866fbfa48f0c4d4fa
Saad Belkhou <saad.belkhou@gmail.com>
2022-12-01 17:07:42 +0100

    changed User entity

--- userService/src/main/java/com/example/userservice/entities/User.java
+++ userService/src/main/java/com/example/userservice/entities/User.java
@@ -1,9 +1,10 @@
 package com.example.userservice.entities;
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@Table(name = "user", schema = "seminarUserService")
+@Data
 public class User {
 
     @Id
@@ -18,84 +19,4 @@ public class User {
 
     private String lastName;
 
-
-    @Id
-    @GeneratedValue(
-            strategy = GenerationType.AUTO,
-            generator = "user_id"
-    )
-    @SequenceGenerator(
-            name = "user_id",
-            sequenceName = "seminarUserService.seq_nutzer_id",
-            allocationSize = 1
-    )
-    @Column(name = "user_id")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "email")
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-
-    }
-
-    @Basic
-    @Column(name = "passwort")
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-
-    }
-
-
-    @Basic
-    @Column(name = "stadt")
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-    @Basic
-    @Column(name = "land")
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    @Basic
-    @Column(name = "vorname")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    @Basic
-    @Column(name = "nachname")
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
