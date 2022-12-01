e06eba70784b6ecc2ab4168ec80b72ec09f8ba54
Saad Belkhou <saad.belkhou@gmail.com>
2022-12-01 17:08:45 +0100

    added annotation

--- userService/src/main/java/com/example/userservice/repositories/UserRepository.java
+++ userService/src/main/java/com/example/userservice/repositories/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface UserRepository extends ReactiveCrudRepository<User,Integer> {
 
 }
